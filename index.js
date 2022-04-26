de19306e9a247b267d7c324d7a62ca255c0c8f2b
Captain Snowball <22148496+SecureSnowball@users.noreply.github.com>
2022-04-26 23:16:45 +0530

    Bug fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const { v5: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 const { port } = require('./src/config/constants');
 const logger = require('./src/util/logger');
 
@@ -10,13 +10,19 @@ const telegramRoute = require('./src/routes/telegram.routes');
 const app = express();
 
 app.use(express.json());
-app.use((req, _, next) => {
+app.use((req, res, next) => {
   req.id = uuid();
+  res.set('X-Request-Id', req.id);
   return next();
 });
 
 // Routes registration
 app.use('/api/telegram', telegramRoute);
+
+// 404 handler
+app.use((_, res) => {
+  res.status(404).json({ message: "Sorry can't find that!" });
+});
 
 app.listen(port, () => {
   logger.info(`Server running on port ${port}`);
--- src/controllers/telegram.controller.js
+++ src/controllers/telegram.controller.js
@@ -6,9 +6,14 @@ class TelegramController {
   }
 
   async webhookHandler(req, res) {
-    const input = req.body;
-    await this.service.greet(input.message.chat_id);
-    return res.json({ message: 'Accepted' });
+    try {
+      const input = req.body;
+      const chatId = input.message.chat_id;
+      await this.service.greet(chatId);
+      return res.json({ message: 'Accepted' });
+    } catch (e) {
+      return res.status(500).json({ message: e.message });
+    }
   }
 }
 
